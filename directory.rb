77164a06705603630ea2b00cffbba5df0c9bcb1b
StefEmp <stephane.emptage@hotmail.o.uk>
2019-08-02 10:49:00 +0100

    added print

--- directory.rb
+++ directory.rb
@@ -11,3 +11,8 @@ puts "Freedy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+# finally, we print the total number of students
+print "Overall, we have "
+# it's imoprtant that print() doesn't add new line characters
+print 11
+puts " great students"
